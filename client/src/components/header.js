1431bf967947db4a3b58e2e8009077641f7f4c7a
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-01-12 00:49:36 -0500

    remove submodule

---
+++ client/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Typography } from '@material-ui/core';
+
+const Header = () => {
+
+  return (
+    <Typography variant='h1' align='center'>Who's That Pokemon?!</Typography>
+  )
+}
+
+export default Header
